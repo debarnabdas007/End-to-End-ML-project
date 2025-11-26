a8eabf83d15d4fdab4c3f3e7730256f528aad2d2
Debarnab <dasbabua321@gmail.com>
2025-11-26 13:21:41 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -91,15 +91,17 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
-
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+
             best_model = models[best_model_name]
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
+
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -113,7 +115,5 @@ class ModelTrainer:
             
 
 
-
-            
         except Exception as e:
             raise CustomException(e,sys)
